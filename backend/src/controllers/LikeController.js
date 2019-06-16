ed059d1e6bf2388f5d3f5552d7dd8c2a67a92607
Ygor Azambuja <ygorazambuja@gmail.com>
2019-06-15 20:14:54 -0400

    corrindo bug backend, postando mobile

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -5,7 +5,7 @@ module.exports = {
 		post.likes += 1;
 		await post.save();
 
-		req.io.emit("post", post);
+		req.io.emit("like", post);
 
 		return res.json(post);
 	},
--- backend/src/index.js
+++ backend/src/index.js
@@ -8,17 +8,18 @@ const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
+mongoose.connect(db.URL, { useNewUrlParser: true });
+
 app.use((req, res, next) => {
 	req.io = io;
 	next();
 });
 
-mongoose.connect(db.URL, { useNewUrlParser: true });
-
 app.use(cors());
-app.use(require("./routes"));
 app.use(
 	"/files",
 	express.static(path.resolve(__dirname, "..", "uploads", "resized")),
 );
+app.use(require("./routes"));
+
 server.listen(3333);
