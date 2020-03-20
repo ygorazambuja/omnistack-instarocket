3e87581a10cf0bceb1485d938b193dfabce15e77
Ygor AZambuja <ygorazambuja@gmail.com>
2020-03-20 14:28:12 -0400

    trocando rotas

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,15 +11,15 @@ const io = require("socket.io")(server);
 mongoose.connect(db.URL, { useNewUrlParser: true });
 
 app.use((req, res, next) => {
-	req.io = io;
-	next();
+  req.io = io;
+  next();
 });
 
 app.use(cors());
 app.use(
-	"/files",
-	express.static(path.resolve(__dirname, "..", "uploads", "resized")),
+  "/files",
+  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
 );
 app.use(require("./routes"));
 
-server.listen(3333);
+server.listen(3000);
